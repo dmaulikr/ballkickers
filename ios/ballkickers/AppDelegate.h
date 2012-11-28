b0adeb037291695400b98b5b0b76bebd6bc31d39
Rudy Jahchan <dev+rudyjahchan@carbonfive.com>
2012-11-28 14:14:43 -0800

    iOS Application

---
+++ ios/ballkickers/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ballkickers
+//
+//  Created by Rudy Jahchan on 11/28/12.
+//  Copyright (c) 2012 Rudy Jahchan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
